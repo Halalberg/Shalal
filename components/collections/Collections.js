2a0722267ffa1bcb2c37b9e2d1f1ca1fc04fe746
Victor Hallberg <81309789+Halalberg@users.noreply.github.com>
2021-11-21 17:00:22 +0100

    Update Collections.js

--- components/collections/Collections.js
+++ components/collections/Collections.js
@@ -5,10 +5,7 @@ import { connect } from 'react-redux';
 import ProductCard from '../products/ProductCard';
 import Pixel from '../components/Pixel'
 
-export default () => 
-  <div>
-    <Pixel name='FACEBOOK_PIXEL_1' />
-  </div>
+
 
 class Collections extends Component {
   constructor(props) {
@@ -160,5 +157,8 @@ class Collections extends Component {
   }
 }
 
-export default connect(state => state)(Collections);
+export default connect(state => state)(Collections)() => 
+  <div>
+    <Pixel name='FACEBOOK_PIXEL_1' />
+  </div>;
 
